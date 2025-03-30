6b8012fdcfde7764b0e39eb399eb49582d653373
pandaacolori <federicocredaro@gmail.com>
2025-03-30 14:28:57 +0200

    New version turn switching implemented

--- my-scripts.js
+++ my-scripts.js
@@ -1,3 +1,5 @@
+let xTurn=true
+
 const playerx = document.querySelector(".playerOne")
   playerx.addEventListener("click", () => {
     let firstPlayer = prompt("What is your name?");
@@ -5,9 +7,27 @@ const playerx = document.querySelector(".playerOne")
     playerDiv.innerText = firstPlayer;
 });
 
-const playero= document.querySelector(".playerTwo")
+const playero = document.querySelector(".playerTwo")
   playero.addEventListener("click", () => {
     let secondPlayer = prompt("What is your name?");
-    const playerDiv = document.querySelector("#babs");
+    const playerDiv = document.querySelector("#nans");
     playerDiv.innerText = secondPlayer;
 });
+
+
+
+const gridAll = document.querySelectorAll(".grid")
+gridAll.forEach(cell => {
+  cell.addEventListener("click", clickFunction, {once:true})
+  })
+
+function clickFunction(e) {
+  const target = e.target;
+  if (xTurn) {
+    target.innerText = "X";
+  } else {
+    target.innerText = "O";
+  }
+  xTurn = !xTurn;
+}
+
