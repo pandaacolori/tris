c430ba011c09f5fabb707c0a10ebc760a605bb3a
pandaacolori <federicocredaro@gmail.com>
2025-03-31 00:29:07 +0200

    working version

--- my-scripts.js
+++ my-scripts.js
@@ -1,4 +1,5 @@
 let xTurn=true
+const reset = document.querySelector("#reset")
 
 const playerx = document.querySelector(".playerOne")
   playerx.addEventListener("click", () => {
@@ -13,8 +14,28 @@ const playero = document.querySelector(".playerTwo")
     const playerDiv = document.querySelector("#nans");
     playerDiv.innerText = secondPlayer;
 });
+const winCondition = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+let board = ["", "", "", "", "", "", "", "", ""];
 
+function checkWinCondition() {
+  for (let winnies of winCondition) { 
+    const [a, b, c] = winnies;  
 
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      if (xTurn) {
+        const winnerName = document.querySelector("#chics").innerText
+        alert(winnerName + " vince!");
+      }
+      else {
+        const winnerName = document.querySelector("#nans").innerText
+        alert(winnerName + " vince!")
+      }
+      return true;
+    }
+  }
+  return false;
+}
+/*CLICKING FUNCTION*/
 
 const gridAll = document.querySelectorAll(".grid")
 gridAll.forEach(cell => {
@@ -23,11 +44,28 @@ gridAll.forEach(cell => {
 
 function clickFunction(e) {
   const target = e.target;
+  const index = [...gridAll].indexOf(target); /*... makes a node list into an array!*/
   if (xTurn) {
     target.innerText = "X";
+    board[index] = "X";
+    checkWinCondition();
   } else {
     target.innerText = "O";
+    board[index] = "O";
+    checkWinCondition();
   }
+  console.log(board)
   xTurn = !xTurn;
 }
+/*RESET BUTTON*/
+reset.addEventListener("click",() => {
+  gridAll.forEach(cell => {
+    cell.innerText = ""
+    board = ["", "", "", "", "", "", "", "", ""];
+    cell.addEventListener("click", clickFunction, {once:true});
+    })
+  xturn=true;
+
+}
+)
 
